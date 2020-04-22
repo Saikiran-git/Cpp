de851c2b117a55af116f7bf47ec8c9feae284f40
Saikiran-git <60788371+Saikiran-git@users.noreply.github.com>
2020-04-22 20:38:35 +0530

    Add files via upload

---
+++ Arrays/Left rotation of vector using STL command.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	// Consider a vector
+	vector<int> v;
+	int z;
+	for(int i = 0; i < n; i++){
+	    cin >> z;
+	    v.push_back(z);
+	}
+	int rot;
+	cin >> rot;
+	// using STL vector command
+	rotate(v.begin(), v.begin() + rot, v.end()); 
+	for(int i = 0; i < n; i++)
+	    cout << v[i] << " ";
+	return 0;
+}
